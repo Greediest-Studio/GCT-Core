ff321e37fd0089534fb1bdc95fbd5d6d400e2b87
mc_Edwin <99588600+EdwinYoungSteve@users.noreply.github.com>
2026-06-24 21:24:33 +0800

    删除植魔的mixin

--- src/main/java/com/smd/gctcore/common/mixin/MixinConfig.java
+++ src/main/java/com/smd/gctcore/common/mixin/MixinConfig.java
@@ -16,7 +16,6 @@ public class MixinConfig implements ILateMixinLoader {
         addModdedMixinCFG("mixins.gctcore.astralsorcery.json", "astralsorcery");
         addModdedMixinCFG("mixins.gctcore.tconstruct.json", "tconstruct");
         addModdedMixinCFG("mixins.gctcore.enderstorage.json", "enderstorage");
-        addModdedMixinCFG("mixins.gctcore.botania.json", "botania");
         addModdedMixinCFG("mixins.gctcore.extrabotany.json", "extrabotany");
         addModdedMixinCFG("mixins.gctcore.abyssalcraft.json", "abyssalcraft");
         addModdedMixinCFG("mixins.gctcore.moretcon.json", "moretcon");
