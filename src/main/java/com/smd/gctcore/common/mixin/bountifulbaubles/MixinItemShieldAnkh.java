32b01aab87451446f47c32130e1195ffdf9060b5
smd <88483238+x8vxv8x@users.noreply.github.com>
2026-07-31 22:52:22 +0800

    补全安赫盾的免疫蜘蛛网的效果

---
+++ src/main/java/com/smd/gctcore/common/mixin/bountifulbaubles/MixinItemShieldAnkh.java
@@ -0,0 +1,22 @@
+package com.smd.gctcore.common.mixin.bountifulbaubles;
+
+import com.smd.gctcore.common.mixin.vanilla.entity.EntityAccessor;
+import cursedflames.bountifulbaubles.item.ItemShieldAnkh;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemShieldAnkh.class)
+public class MixinItemShieldAnkh {
+
+    @Inject(method = "onWornTick", at = @At("HEAD"), remap = false)
+    private void onOnWornTick(ItemStack stack, EntityLivingBase player, CallbackInfo ci) {
+        if (player instanceof EntityPlayer) {
+            ((EntityAccessor) player).setIsInWeb(false);
+        }
+    }
+}
